97572fbfedb31a011cb69ba8a61eeeb0c397d35a
andersongilvan <andersongilvan@gmail.com>
2025-12-13 10:13:46 -0300

    feat: Inclui e configura o Spring Security na aplicação

---
+++ src/main/java/com/movieflix/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.movieflix.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/movieflix/user").permitAll();
+
+                    auth.anyRequest().authenticated();
+
+                });
+        return httpSecurity.build();
+
+    }
+}
